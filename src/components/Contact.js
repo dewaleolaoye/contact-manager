129dad096265deff02cefd68651e6db2b5828471
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-07 09:53:28 +0100

    contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Adewale Olaoye</h4>
+        <ul>
+          <li>Email: adewaleolaoye21@gamil.com</li>
+          <li>Phone: 07085160602</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
