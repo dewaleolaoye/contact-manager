cd4fc3d529038b147bde33cbb0119b92142bae7e
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-07 17:16:44 +0100

    add fontawesome

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,21 @@ import propTypes from 'prop-types';
 
 class Contact extends React.Component {
   render() {
-    const { contact } = this.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className='card card-body mb-3'>
-        <h4>{contact.name}</h4>
+        <h4>
+          {name} <i className='fas fa-sort-down' />
+        </h4>
         <ul className='list-group'>
-          <li className='list list-group-item'>Email: {contact.email}</li>
-          <li className='list list-group-item'>Phone: {contact.phone}</li>
+          <li className='list list-group-item'>Email: {email}</li>
+          <li className='list list-group-item'>Phone: {phone}</li>
         </ul>
       </div>
     );
   }
 }
 
-// eslint-disable-next-line react/no-typos
 Contact.propTypes = {
   contact: propTypes.object.isRequired
 };
