84eaf0e14863716a7631d20ca427e918d113af64
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-07 18:07:20 +0100

    change state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,17 +2,34 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 class Contact extends React.Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className='card card-body mb-3'>
         <h4>
-          {name} <i className='fas fa-sort-down' />
+          {name}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className='fas fa-sort-down'
+          />
         </h4>
-        <ul className='list-group'>
-          <li className='list list-group-item'>Email: {email}</li>
-          <li className='list list-group-item'>Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className='list-group'>
+            <li className='list list-group-item'>Email: {email}</li>
+            <li className='list list-group-item'>Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
